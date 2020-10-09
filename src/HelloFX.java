1b40e2504abdf85058669f51407bd7097979fb93
physcrowley <crowlda@ecolecatholique.ca>
2020-10-09 12:30:11 -0400

    changed variable name Label l > Label txt

--- src/HelloFX.java
+++ src/HelloFX.java
@@ -23,8 +23,8 @@ public class HelloFX extends Application {
         String javafxVersion = System.getProperty("javafx.version");
 
 
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Label txt = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(txt), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
