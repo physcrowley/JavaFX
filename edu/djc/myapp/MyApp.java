dcc0598b6e3162cc94679e0dc64de42cb14fa1af
physcrowley <crowlda@ecolecatholique.ca>
2020-10-18 16:02:29 -0400

    added MyApp example from Application javadoc

---
+++ edu/djc/myapp/MyApp.java
@@ -0,0 +1,21 @@
+package edu.djc.myapp;
+ 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class MyApp extends Application 
+{
+    public void start(Stage stage) 
+    {
+        Circle circ = new Circle(40, 40, 30);
+        Group root = new Group(circ);
+        Scene scene = new Scene(root, 400, 300);
+
+        stage.setTitle("My JavaFX Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
