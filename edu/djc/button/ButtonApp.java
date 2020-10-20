77ac29761ebc1f7bc759df941de23b86be7a71ae
physcrowley <crowlda@ecolecatholique.ca>
2020-10-19 23:30:22 -0400

    added ButtonApp.java

---
+++ edu/djc/button/ButtonApp.java
@@ -0,0 +1,14 @@
+package edu.djc.button;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ButtonApp extends Application {
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
