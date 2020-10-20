3423456fe982e918df4743dd536de1ed4c4d16a0
physcrowley <crowlda@ecolecatholique.ca>
2020-10-20 12:15:48 -0400

    attempt at integrating SceneBuilder and fxml

--- edu/djc/hello/HelloApp.java
+++ edu/djc/hello/HelloApp.java
@@ -1,21 +1,36 @@
 package edu.djc.hello;
 
+import javax.tools.DocumentationTool.Location;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class HelloApp extends Application {
-
-    private Parent createContent() {
-        return new StackPane(new Text("Hello World"));
+public class HelloApp extends Application
+{    
+    private Parent loadDesign(String fxml)
+    {
+        try  
+            {return FXMLLoader.load(getClass().getResource(fxml));}
+        catch(Exception e)
+        {
+            System.out.println(getClass());
+            System.out.println(getClass().getResource(fxml));
+            System.out.println("Fxml loading error " + e);
+            return (Parent)null;
+        }
     }
+
+    Parent root = loadDesign("UI.fxml");
+    
+    //FXMLLoader fxml = new FXMLLoader(getClass().getResource("UI.fxml"));
+     //Parent root = fxml.load();
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent(), 300, 300));
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
