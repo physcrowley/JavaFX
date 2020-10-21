5f9dfb731c8112dacd7fcddc1b14cb3ccdd36c7f
physcrowley <crowlda@ecolecatholique.ca>
2020-10-21 14:32:26 -0400

    add ButtonApp example with lambda expressions

--- edu/djc/button/ButtonApp.java
+++ edu/djc/button/ButtonApp.java
@@ -1,14 +1,46 @@
 package edu.djc.button;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
+ 
 public class ButtonApp extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        // TODO Auto-generated method stub
-
+    public static void main(String[] args) {
+        launch(args);
     }
     
+    public void buttonClick()
+    {
+        System.out.println("Hello World!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+
+        btn.setOnAction(event -> System.out.println("Hello World!")); //expression lambda
+        
+        //btn.setOnAction(event -> buttonClick()); //expression lambda qui appelle une autre fonction
+
+        /*
+        btn.setOnAction(new EventHandler<ActionEvent>() { // déclaration de classe anonyme
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        */
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
