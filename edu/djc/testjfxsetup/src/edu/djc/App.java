77c4599b14a3f2b51045c87ea3dee70aebb96191
physcrowley <crowlda@ecolecatholique.ca>
2020-10-21 16:48:14 -0400

    testing some launch.json parameters for fxml projects

---
+++ edu/djc/testjfxsetup/src/edu/djc/App.java
@@ -0,0 +1,24 @@
+package edu.djc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(App.class.getResource("primary.fxml"));
+        
+        var scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+}
